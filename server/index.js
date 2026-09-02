61c94714383dd17a838723ea26e2fc4e62e94f09
AryanBhatare07 <aryanbhatare07@gmail.com>
2026-09-03 01:07:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://notesgenix-aiclient.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
